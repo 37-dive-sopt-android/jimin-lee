4486f3eca12ab89111011c86b5ccfaed90d56d00
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2025-11-06 00:23:04 +0900

    #5/feat: card flip 구현

--- app/src/main/java/com/sopt/dive/ui/screen/search/SearchScreen.kt
+++ app/src/main/java/com/sopt/dive/ui/screen/search/SearchScreen.kt
@@ -1,18 +1,31 @@
 package com.sopt.dive.ui.screen.search
 
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.sopt.dive.R
+import com.sopt.dive.ui.screen.search.CardFront
 
 @Composable
 fun SearchScreen(
@@ -21,22 +34,67 @@ fun SearchScreen(
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .padding(innerPadding)
+            .padding(innerPadding),
+        contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = stringResource(R.string.title_Search),
-            modifier = Modifier
-                .align(Alignment.TopCenter)
-                .padding(top = 26.dp),
-            fontSize = 30.sp,
-            fontWeight = FontWeight.Bold
+
+        var isFlipped by remember { mutableStateOf(false) }
+        val rotation by animateFloatAsState(
+            targetValue = if (isFlipped) 180f else 0f,
+            animationSpec = tween(
+                durationMillis = 600,
+                easing = FastOutSlowInEasing
+            )
         )
 
-        Text(
-            text = "안녕하세요!!",
+        Card(
             modifier = Modifier
-                .align(Alignment.Center),
-            fontSize = 15.sp,
-        )
+                .width(200.dp)
+                .aspectRatio(0.75f)
+                .graphicsLayer {
+                    rotationY = rotation
+                    cameraDistance = 16 * density
+                },
+            onClick = { isFlipped = !isFlipped }
+        ) {
+            if (rotation <= 90f) {
+                CardFront()
+            } else {
+                CardBack(
+                    modifier = Modifier.graphicsLayer {
+                        rotationY = 180f
+                    }
+                )
+            }
+        }
     }
+}
+
+@Composable
+private fun CardFront(){
+    Image(
+        painter = painterResource(R.drawable.card_front),
+        contentDescription = null,
+        modifier = Modifier.fillMaxSize(),
+        contentScale = ContentScale.Crop
+    )
+}
+
+@Composable
+private fun CardBack(
+    modifier: Modifier = Modifier
+){
+    Image(
+        painter = painterResource(R.drawable.card_back),
+        contentDescription = null,
+        modifier = modifier.fillMaxSize(),
+        contentScale = ContentScale.Crop
+
+    )
+}
+
+@Preview
+@Composable
+private fun SearchScreenPreview() {
+    SearchScreen(PaddingValues())
 }
