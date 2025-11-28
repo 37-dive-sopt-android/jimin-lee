43565bcd93d539b666a3f64130ddb9f95ccf543d
vahkjsdf <vahkjsdf@sookmyung.ac.kr>
2025-11-28 20:17:24 +0900

    [feat/#9] MyRoute 추가

--- app/src/main/java/com/sopt/dive/ui/screen/my/MyScreen.kt
+++ app/src/main/java/com/sopt/dive/ui/screen/my/MyScreen.kt
@@ -32,30 +32,29 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import com.sopt.dive.R
 import com.sopt.dive.ui.common.UiState
 import com.sopt.dive.data.local.SharedPreference
+import com.sopt.dive.ui.screen.LoadingScreen
 import com.sopt.dive.ui.screen.my.component.Info
 
 @Composable
-fun MyScreen(
+fun MyRoute(
     innerPadding: PaddingValues,
     viewModel: MyViewModel = viewModel(),
 ) {
-    val myState by viewModel.myState.collectAsStateWithLifecycle()
-
     val context = LocalContext.current
+
     val sharedPref = SharedPreference(context)
     val userPathId by rememberSaveable { mutableStateOf(sharedPref.getUserId().userId) }
+
+    val myState by viewModel.myState.collectAsStateWithLifecycle()
 
     LaunchedEffect(userPathId) {
         viewModel.getUserData(userPathId)
     }
 
     when (val state = myState) {
-        is UiState.Loading -> {
-            LoadingMyScreen(innerPadding)
-        }
         is UiState.Success -> {
             val user = state.data
-            SuccessMyScreen(
+            MyScreen(
                 userUName = user?.username ?: "",
                 userName = user?.name ?: "",
                 userEmail = user?.email ?: "",
@@ -66,26 +65,14 @@ fun MyScreen(
         is UiState.Failure -> {
             Toast.makeText(context, "내 정보를 불러올 수 없습니다.", Toast.LENGTH_SHORT).show()
         }
+        is UiState.Loading -> {
+            LoadingScreen(innerPadding)
+        }
     }
 }
 
 @Composable
-private fun LoadingMyScreen(
-    innerPadding: PaddingValues
-) {
-    Column(
-        modifier = Modifier
-            .padding(innerPadding)
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        CircularProgressIndicator()
-    }
-}
-
-@Composable
-private fun SuccessMyScreen (
+private fun MyScreen (
     userName: String,
     userUName: String,
     userEmail: String,
